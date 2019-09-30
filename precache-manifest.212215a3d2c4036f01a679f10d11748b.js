0294ed55093102be55706185db84833d721e31a5
chenwuliang <leonsin@163.com>
2019-09-30 07:03:41 +0000

    Deploying to gh-pages from master 3a4002a31789dbad3762a26965530b1343be4cca

---
+++ precache-manifest.212215a3d2c4036f01a679f10d11748b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9a74b4571d44d84a2c2b8fc3c50204",
+    "url": "/Words/index.html"
+  },
+  {
+    "revision": "af7bc75afbf8910323bb",
+    "url": "/Words/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "11db071cde1d7872de31",
+    "url": "/Words/static/js/2.9b0a676c.chunk.js"
+  },
+  {
+    "revision": "af7bc75afbf8910323bb",
+    "url": "/Words/static/js/main.6116007d.chunk.js"
+  },
+  {
+    "revision": "d15e5521339e27de58b8",
+    "url": "/Words/static/js/runtime-main.7e24ea4b.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Words/static/media/logo.25bf045c.svg"
+  }
+]);
